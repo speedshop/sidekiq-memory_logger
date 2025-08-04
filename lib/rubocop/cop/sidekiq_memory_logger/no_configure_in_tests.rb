09c56ea63c7deaf5aa8e178168e44388caf8466d
Nate Berkopec <nate.berkopec@gmail.com>
2025-08-04 10:19:18 +0900

    Refactor custom RuboCop cop to use configuration instead of hardcoded file pattern
    - Remove in_test_file? method from NoConfigureInTests cop
    - Use RuboCop's Include configuration to specify test files pattern
    - Maintains same functionality while being more configurable

--- lib/rubocop/cop/sidekiq_memory_logger/no_configure_in_tests.rb
+++ lib/rubocop/cop/sidekiq_memory_logger/no_configure_in_tests.rb
@@ -7,18 +7,12 @@ module RuboCop
         MSG = "Do not use Sidekiq::MemoryLogger.configure in test files. Use Configuration objects instead."
 
         def on_send(node)
-          return unless in_test_file?
-
           if sidekiq_memory_logger_configure?(node)
             add_offense(node)
           end
         end
 
         private
-
-        def in_test_file?
-          processed_source.path.match?(%r{test/.*\.rb\z})
-        end
 
         def sidekiq_memory_logger_configure?(node)
           node.send_type? &&
